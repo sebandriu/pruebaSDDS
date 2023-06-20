639343f3523daaad4ac365c0b0f2b622f6427ceb
sebandriu <sebaandresrobles@live.cl>
2023-06-20 17:42:58 -0400

    conexion BDD

--- db-connect.php
+++ db-connect.php
@@ -7,8 +7,16 @@ $port = "3306";
 $sslmode = "require";
 $flag = "MYSQLI_CLIENT_SSL";
 
-$conn = new mysqli($hostname, $username, $password, $db_name, $port, $sslmode, $flag);
+/*$conn = new mysqli($hostname, $username, $password, $db_name, $port, $sslmode, $flag);
+if (!$conn) {
+   die('Database connection failed');
+}*/
+
+$conn = mysqli_init();
+mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+mysqli_real_connect($conn, "gestordecitasdemo-server.mysql.database.azure.com", "dptkvmzuil", "Z5UE64DVF78R082Y$", "members", 3306, MYSQLI_CLIENT_SSL);
 if (!$conn) {
    die('Database connection failed');
 }
+
 ?>
