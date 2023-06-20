b85c409d3fce649b79cc0ea4277d28dded602995
sebandriu <sebaandresrobles@live.cl>
2023-06-19 23:46:33 -0400

    conexion
    azure conexion

--- db-connect.php
+++ db-connect.php
@@ -1,8 +1,8 @@
 <?php
-$host = "134.209.191.58";
-$username = "hvewsyfpgs";
-$pw = "3xeDmtDX8y";
-$db_name = "hvewsyfpgs";
+$host = "gestordecitasdemo-server.mysql.database.azure.com";
+$username = "dptkvmzuil";
+$pw = "Z5UE64DVF78R082Y$";
+$db_name = "members";
 
 $conn = new mysqli($host, $username, $pw, $db_name);
 if (!$conn) {
