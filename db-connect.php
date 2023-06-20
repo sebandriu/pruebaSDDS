e7eb75694113be43d408d7534ab021e09564d33c
sebandriu <sebaandresrobles@live.cl>
2023-06-20 18:09:20 -0400

    Update db-connect.php

--- db-connect.php
+++ db-connect.php
@@ -4,8 +4,6 @@ $username = "dptkvmzuil";
 $password = "Z5UE64DVF78R082Y$";
 $db_name = "members";
 $port = "3306";
-$sslmode = "require";
-$flag = "MYSQLI_CLIENT_SSL";
 
 /*$conn = new mysqli($hostname, $username, $password, $db_name, $port, $sslmode, $flag);
 if (!$conn) {
@@ -13,7 +11,7 @@ if (!$conn) {
 }*/
 
 $conn = mysqli_init();
-mysqli_ssl_set($conn,NULL,NULL, "/certificado/ca.pem", NULL, NULL);
+mysqli_ssl_set($conn,NULL,NULL, "ca.pem", NULL, NULL);
 mysqli_real_connect($conn, "gestordecitasdemo-server.mysql.database.azure.com", "dptkvmzuil", "Z5UE64DVF78R082Y$", "members", 3306, MYSQLI_CLIENT_SSL);
 if (!$conn) {
    die('Database connection failed');
